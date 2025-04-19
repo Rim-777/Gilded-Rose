933439ef5c3b79f0b1906bffa3dc377ecba6f66c
Timo Moss <rails.moss@gmail.com>
2025-04-19 22:14:06 +0100

    refactor GildedRose module

--- lib/gilded_rose.ex
+++ lib/gilded_rose.ex
@@ -1,136 +1,161 @@
 defmodule GildedRose do
-  # Example
-  # update_quality([%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 9, quality: 1}])
-  # => [%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 8, quality: 3}]
+  @moduledoc """
+  Provides functionality to update the quality and sell-in values of items in the Gilded Rose inventory system.
 
+  This module handles various types of items, including regular items, aged brie, backstage passes, sulfuras,
+  and conjured items. It applies specific rules to update the quality and sell-in values based on the type of item.
+  """
+
+  import GildedRose.ItemTypes
+
+  @max_quality 50
+
+  @doc """
+  Updates the quality and sell-in values for a list of items.
+
+  Parameters:
+    - items: A list of `%Item{}` structs representing the items to be updated.
+  Returns:
+    - A list of `%Item{}` structs with updated quality and sell-in values.
+
+  ## Example
+    iex> GildedRose.update_quality([%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 9, quality: 1}])
+    [%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 8, quality: 3}]
+  """
   def update_quality(items) do
     Enum.map(items, &update_item/1)
   end
 
-  defp step_1(item) do
-    cond do
-      item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" ->
-        if item.quality > 0 do
-          if item.name != "Sulfuras, Hand of Ragnaros" do
-            %{item | quality: item.quality - 1}
-          else
-            item
-          end
-        else
-          item
-        end
-
-      true ->
-        cond do
-          item.quality < 50 ->
-            item = %{item | quality: item.quality + 1}
-
-            cond do
-              item.name == "Backstage passes to a TAFKAL80ETC concert" ->
-                item =
-                  cond do
-                    item.sell_in < 11 ->
-                      cond do
-                        item.quality < 50 ->
-                          %{item | quality: item.quality + 1}
-
-                        true ->
-                          item
-                      end
-
-                    true ->
-                      item
-                  end
-
-                cond do
-                  item.sell_in < 6 ->
-                    cond do
-                      item.quality < 50 ->
-                        %{item | quality: item.quality + 1}
-
-                      true ->
-                        item
-                    end
-
-                  true ->
-                    item
-                end
-
-              true ->
-                item
-            end
-
-          true ->
-            item
-        end
-    end
+  defp update_item(%Item{} = item) do
+    item
+    |> decrease_sell_in()
+    |> update_decreasable_item()
+    |> update_increasable_item()
+    |> update_eventtable_item()
+    |> update_conjured_item?()
   end
 
-  defp step_2(item) do
-    cond do
-      item.name != "Sulfuras, Hand of Ragnaros" ->
-        %{item | sell_in: item.sell_in - 1}
-
+  defp update_decreasable_item(item) do
+    case decreasable_type?(item) do
       true ->
-        item
-    end
-  end
-
-  defp step_3(item) do
-    cond do
-      item.sell_in < 0 ->
-        cond do
-          item.name != "Aged Brie" ->
-            cond do
-              item.name != "Backstage passes to a TAFKAL80ETC concert" ->
-                cond do
-                  item.quality > 0 ->
-                    cond do
-                      item.name != "Sulfuras, Hand of Ragnaros" ->
-                        %{item | quality: item.quality - 1}
-
-                      true ->
-                        item
-                    end
-
-                  true ->
-                    item
-                end
-
-              true ->
-                %{item | quality: item.quality - item.quality}
-            end
-
-          true ->
-            cond do
-              item.quality < 50 ->
-                %{item | quality: item.quality + 1}
-
-              true ->
-                item
-            end
-        end
-
-      true ->
-        item
-    end
-  end
-
-  defp step_4(item) do
-    case String.contains?(item.name, "Conjured") do
-      true ->
-        %{item | quality: item.quality - 1}
+        decrease_with_negative_sell_in(item)
 
       false ->
         item
     end
   end
 
-  def update_item(item) do
-    item
-    |> step_1()
-    |> step_2()
-    |> step_3()
-    |> step_4()
+  defp decrease_with_negative_sell_in(item) do
+    case item.sell_in < 0 do
+      true ->
+        decrease_quality(item)
+
+      false ->
+        item
+    end
+    |> decrease_quality()
+  end
+
+  defp update_increasable_item(item) do
+    case increasable_type?(item) do
+      true ->
+        increase_with_negative_sell_in(item)
+
+      false ->
+        item
+    end
+  end
+
+  defp increase_with_negative_sell_in(item) do
+    case item.sell_in < 0 do
+      true ->
+        increase_quality(item)
+
+      false ->
+        item
+    end
+    |> increase_quality()
+  end
+
+  defp update_eventtable_item(item) do
+    case eventable_type?(item) do
+      true ->
+        item
+        |> update_eventable_item()
+        |> update_upcoming_eventable_item()
+        |> update_imminent_eventable_item()
+        |> update_passed_eventable_item()
+
+      false ->
+        item
+    end
+  end
+
+  defp update_eventable_item(item) do
+    case item.quality < @max_quality do
+      true ->
+        increase_quality(item)
+
+      false ->
+        item
+    end
+  end
+
+  defp update_upcoming_eventable_item(item) do
+    case item.quality < @max_quality and item.sell_in < 11 do
+      true ->
+        increase_quality(item)
+
+      false ->
+        item
+    end
+  end
+
+  defp update_imminent_eventable_item(item) do
+    case item.quality < @max_quality and item.sell_in < 6 do
+      true ->
+        increase_quality(item)
+
+      false ->
+        item
+    end
+  end
+
+  defp update_passed_eventable_item(item) do
+    case item.sell_in < 0 do
+      true ->
+        %{item | quality: 0}
+
+      false ->
+        item
+    end
+  end
+
+  defp decrease_sell_in(item) do
+    case unsaleable_type?(item) do
+      true ->
+        item
+
+      false ->
+        %{item | sell_in: item.sell_in - 1}
+    end
+  end
+
+  defp update_conjured_item?(item) do
+    case conjured_type?(item) do
+      true ->
+        decrease_quality(item, 2)
+
+      false ->
+        item
+    end
+  end
+
+  defp increase_quality(item, delta \\ 1) do
+    %{item | quality: item.quality + delta}
+  end
+
+  defp decrease_quality(item, delta \\ 1) do
+    %{item | quality: item.quality - delta}
   end
 end
